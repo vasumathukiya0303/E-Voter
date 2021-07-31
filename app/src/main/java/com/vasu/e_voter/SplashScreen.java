2c4dcf5c1edafdd40b9f934e54ada8c90aab0d38
VasuMathukiya0303 <vmathukiya183@rku.ac.in>
2021-07-31 17:27:26 +0530

    Login As screen change on main activity only designe view

--- app/src/main/java/com/vasu/e_voter/SplashScreen.java
+++ app/src/main/java/com/vasu/e_voter/SplashScreen.java
@@ -1,14 +1,26 @@
 package com.vasu.e_voter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
